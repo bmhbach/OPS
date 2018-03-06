3a6e351c5be53ab8a1c9d32a460dd81572f19674
Anirudh Balasubramaniam <anirudhnkl@gmail.com>
2018-03-05 18:15:15 -0800

    Checkpoint 2

---
+++ Radio/Checkpoint_2/Transmitter/transmitter_final/transmitter_final.ino
@@ -0,0 +1,50 @@
+#include "transmitter.h"
+
+#define switchA 19
+#define switchB 17
+#define switchC 15
+
+int buttonStateA = 0;
+int buttonStateB = 0;
+int buttonStateC = 0;
+
+void setup() {
+  Serial.begin(9600);
+  transmitter_setup();
+  pinMode(switchA, INPUT);  
+  pinMode(switchB, INPUT);  
+  pinMode(switchC, INPUT);  
+}
+
+void loop() {
+  if (buttonStateA == LOW && digitalRead(switchA) == HIGH) //if previously off, communicate it is on when on
+  {
+    sending(1);
+    //Serial.write(1);
+    delay(100);
+    buttonStateA = HIGH;
+  }
+  else if (digitalRead(switchA) == LOW) //if previously on, turn off when switch is released
+    buttonStateA = LOW;
+ 
+  if (buttonStateB == LOW && digitalRead(switchB) == HIGH)
+  {
+    sending(2);
+    //Serial.write(2);
+    delay(100);
+    buttonStateB = HIGH;
+  }
+  else if (digitalRead(switchB) == LOW)
+    buttonStateB = LOW;
+  
+  if (buttonStateC == LOW && digitalRead(switchC) == HIGH)
+  {
+    sending(3);
+    //Serial.write(3);
+    delay(100);
+    buttonStateC = HIGH;
+  }
+  if (digitalRead(switchC) == LOW)
+    buttonStateC = LOW;
+  
+}
